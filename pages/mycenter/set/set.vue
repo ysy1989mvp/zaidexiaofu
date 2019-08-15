93b15fe1fb244c339f45a8cce62907f246f2ead6
liuxinyu <lololoy@163.com>
2019-08-15 23:01:39 +0800

    啊打发

---
+++ pages/mycenter/set/set.vue
@@ -0,0 +1,130 @@
+<template>
+	<view class="container">
+		<view class="content">
+			<view class="part1">
+				<view class="part1a">
+					<image class="TXA" src="../../../static/ysy/toux.png"></image>
+					<view class="zl">
+						<view class="number">16852028713</view>
+						<view class="school">昆明市第一小学</view>
+					</view>
+					<view class="bj">编辑</view>
+				</view>
+			</view>
+			<view class="part2">
+				<view class="fw dizhi1">
+					<view class="ee">
+						<view class="c1">我的收货地址</view>
+						<view class="c2">〉</view>
+					</view>
+				</view>
+				<view class="fw">
+					<view class="ee">
+						<view class="c1">账户与安全</view>
+						<view class="c2">〉</view>
+					</view>
+				</view><view class="fw">
+					<view class="ee">
+						<view class="c1">问题反馈</view>
+						<view class="c2">〉</view>
+					</view>
+				</view><view class="fw">
+					<view class="ee">
+						<view class="c1">关于载德</view>
+						<view class="c2">〉</view>
+					</view>
+				</view>
+				</view><view class="fw butn login">
+						账号切换
+				</view>
+				</view><view class="fw butn">
+						退出登录
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: #E4E7ED;
+	}
+
+	.TXA {
+		height: 110upx;
+		width: 130upx;
+	}
+
+	.zl {
+		font-size: 28upx;
+		width: 60%;
+		line-height: 40upx;
+	}
+
+	.part1 {
+		/* display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between; */
+		font-size: 28upx;
+		/* margin: 20upx 30upx;	 */
+		background-color: #FFFFFF;
+	}
+	.part1a{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		margin: 10upx 30upx;
+	}
+
+	.bj {
+		border-radius: 10upx;
+		border: 2upx solid #000000;
+		padding: 0upx 20upx;
+	}
+
+	.ee {
+		display: flex;
+		flex-direction: row;
+		margin-top: 15upx;
+		margin-bottom: 15upx;
+		height: 80upx;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 28upx;
+		width: 90%;
+	}
+	.fw{
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #FFFFFF;
+		border-bottom: 2upx solid #A5A5A5;
+	}
+	.dizhi1{
+		margin-bottom: 35upx;
+	}
+	.butn{
+		height: 80upx;
+		margin-top: 30upx;
+		font-size: 30upx;
+	}
+	.login{
+		
+	}
+</style>
